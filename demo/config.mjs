63a837e978c5acd8c9e9a51e934373775de23808
Aniket Kumar <aniketkumar049@gmail.com>
2021-01-09 23:29:06 +0530

    config added

---
+++ demo/config.mjs
@@ -0,0 +1,4 @@
+export const authorizationUserId = 'User1';
+export const password = '1234';
+export const realm = 'asterisk.dialer.dpi.one';
+export const websocketUrl = 'wss://asterisk.dialer.dpi.one:8089/ws';
